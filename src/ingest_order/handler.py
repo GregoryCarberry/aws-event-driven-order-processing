79dcd80746d501ec21e26a8cc053786819399b41
GregoryCarberry <gjc01@hotmail.co.uk>
2026-06-12 23:08:51 +0100

    fix: correct Lambda placeholder indentation

--- src/ingest_order/handler.py
+++ src/ingest_order/handler.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
-"""Placeholder ingestion Lambda handler."""
-return {
-"statusCode": 501,
-"body": "ingest_order handler not implemented yet"
-}
+    """Placeholder ingestion Lambda handler."""
+    return {
+        "statusCode": 501,
+        "body": "ingest_order handler not implemented yet"
+    }
